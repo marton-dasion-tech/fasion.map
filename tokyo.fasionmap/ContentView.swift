39b9ca24f2e8af4a7147d3cbe591479f0d198998
maruyama kenya <ken04.maruym.buss@gmail.com>
2026-04-29 10:22:00 -0700

    Initial Commit

---
+++ tokyo.fasionmap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tokyo.fasionmap
+//
+//  Created by 丸山堅也 on 2026/04/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tokyo.fasionmap/tokyo_fasionmapApp.swift
@@ -0,0 +1,17 @@
+//
+//  tokyo_fasionmapApp.swift
+//  tokyo.fasionmap
+//
+//  Created by 丸山堅也 on 2026/04/29.
+//
+
+import SwiftUI
+
+@main
+struct tokyo_fasionmapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
